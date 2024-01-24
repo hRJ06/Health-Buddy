750cdbe350654183fc6dc14aed15d8dcdd6e2435
hRJ06 <hindolroy.2306@gmail.com>
2024-01-25 02:36:41 +0530

    Updated Pet Controller

--- Hindol/Server/controller/Pet.js
+++ Hindol/Server/controller/Pet.js
@@ -1,20 +1,27 @@
-const { default: mongoose } = require('mongoose');
-const Pet = require('../model/Pet');
-exports.getAppointments = async(req, res) => {
-    try {
-        const {petId} = req.body;
-        const pet = await Pet.findById({_id: new mongoose.Types.ObjectId(petId)}).populate('appointments').exec();
-        return res.status(200).json({
-            success: true,
-            data: pet,
-            message: 'Fetched appointments for pet'
-        })
-    }
-    catch (err) {
-        console.error(err);
-        return res.status(500).json({
-            success: false,
-            message: 'Unable to get Appointments of Pet'
-        })
-    }
-}
+const { default: mongoose } = require("mongoose");
+const Pet = require("../model/Pet");
+exports.getAppointments = async (req, res) => {
+  try {
+    const { petId } = req.body;
+    const pet = await Pet.findById({ _id: new mongoose.Types.ObjectId(petId) })
+      .populate({
+        path: "appointments",
+        populate: [
+          { path: "doctor" },
+          { path: "slot" },
+        ],
+      })
+      .exec();
+    return res.status(200).json({
+      success: true,
+      data: pet,
+      message: "Fetched appointments for pet",
+    });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({
+      success: false,
+      message: "Unable to get Appointments of Pet",
+    });
+  }
+};
