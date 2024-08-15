7473acef9dc86fe59231d8b029c56a4b19d2e85e
hRJ06 <hindolroy.2306@gmail.com>
2024-08-16 00:30:50 +0530

    Updated Server

--- Hindol/src/data/doctorType.js
+++
@@ -1,4 +0,0 @@
-export const doctorTypeOptions = [
-    {value: 'dog' , label: 'Dog'},
-    {value: 'cat' , label: 'Cat'}   
-]
--- Hindol/src/data/petType.js
+++
@@ -1,4 +0,0 @@
-export const petType = [
-    {value: 'dog' , label: 'Dog'},
-    {value: 'cat' , label: 'Cat'}   
-]
